a4725b0bf31a8c051e1aa1ba497d5d288d7fef66
dapras2000 <dapras.solo@gmail.com>
2023-08-02 14:32:01 +0700

    my-quasar-todo

---
+++ src/pages/Help.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-pa-lg"
+    ><h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam accusamus
+      aliquam repellendus excepturi vel ab necessitatibus sint beatae numquam
+      perferendis quam alias laborum ducimus, possimus quos eos? Quos, placeat
+      nam.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam accusamus
+      aliquam repellendus excepturi vel ab necessitatibus sint beatae numquam
+      perferendis quam alias laborum ducimus, possimus quos eos? Quos, placeat
+      nam.
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelpPage",
+});
+</script>
